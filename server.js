954e3366712250a236bc38e00fd5eef4b8d932cd
Ikenna Nwajagu <Ikenna.nwajagu@gmail.com>
2020-04-27 22:14:14 -0400

    cleaning up code

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const express = require("express");
 const path = require("path");
 const fs = require("fs");
 
-
 // Sets up the Express App
 // =============================================================
 const app = express();
@@ -12,43 +11,42 @@ const PORT = process.env.PORT || 3000;
 
 let rawnoteData = [];
 
-
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(__dirname + '/public'));
 
-//Routes for HTML
-//--Home Page ROute
+//--------------------ROUTES FOR HTML----------------------------
+//HOME PAGE ROUTE
 app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname, "/public/index.html"));
 });
-//--Notes Route 
+
+//NOTES ROUTE 
 app.get("/notes", function (req, res) {
   res.sendFile(path.join(__dirname, "/public/notes.html"));
 });
 
-//-----API ROUTES-----------
+//----------------------API ROUTES---------------------------------
 //GET ROUTE
 app.get("/api/notes", function (req, res) {
-
   try {
+
     rawnoteData = fs.readFileSync(path.join(__dirname, "./db/db.json"), "utf8");
 
     rawnoteData = JSON.parse(rawnoteData);
   }
   catch (err) {
-    //console.log(err);
     return err;
   }
-  res.json(rawnoteData);
 
+  res.json(rawnoteData);
 });
 
 //POST ROUTE
 app.post("/api/notes", function (req, res) {
-
   try {
+
     rawnoteData = fs.readFileSync(path.join(__dirname, "./db/db.json"), "utf8");
 
     rawnoteData = JSON.parse(rawnoteData);
@@ -62,22 +60,19 @@ app.post("/api/notes", function (req, res) {
     fs.writeFile(path.join(__dirname, "./db/db.json"), rawnoteData, "utf8",
       function (err) {
         if (err) throw err;
-
       });
 
     res.json(JSON.parse(rawnoteData));
-
   }
   catch (err) {
     return err;
   }
-
 });
 
-//Delete Route
-
+//DELETE ROUTE
 app.delete("/api/notes/:id", function (req, res) {
   try {
+
     rawnoteData = fs.readFileSync(path.join(__dirname, "./db/db.json"), "utf8");
 
     rawnoteData = JSON.parse(rawnoteData);
@@ -91,30 +86,16 @@ app.delete("/api/notes/:id", function (req, res) {
     fs.writeFile(path.join(__dirname, "./db/db.json"), rawnoteData, "utf8",
       function (err) {
         if (err) throw err;
-
       });
 
     res.send(JSON.parse(rawnoteData));
-
-
-  }
+}
   catch (err) {
     throw err;
   }
-
 });
 
+//-----STARTS SERVER TO BEGIN LISTENING------------------
 app.listen(PORT, function () {
   console.log("SERVER IS LISTENING: " + PORT);
 });
-
-
-
-
-
-
-
-
-
-
-
